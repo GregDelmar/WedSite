4add1dcbb6a728f422e6729a2fbae88b25a5482c
Greg Delmar <gregdelmar@live.com>
2011-09-28 22:03:20 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WedSite::Application.config.secret_token = 'f870472b128a5be8a43f87f8dfaba3efef23655c13cbe5ae4048080b3d696a9befee0a5acd36e067453aa3c5169a0bdca288fa1398f32330b22237634ca3f8a3'
